d498d507dbd8b93709bbb034b858c9d8a223d6fc
Dra <nd.mirica@gmail.com>
2023-07-12 18:21:56 +0300

    loading updated

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
-import { Loader } from "./components/global/Loader/Loader";
 import { NavigationBar } from "./components/global/NavigationBar/NavigationBar";
 
 const App = () => {
-  //state for the loading of the website
-  const [isLoading, setIsLoading] = useState(true);
-
-  //check if the website is loaded entirely
-  useEffect(() => {
-    const handleWindowLoad = () => {
-      setIsLoading(false);
-    };
-
-    window.addEventListener("load", handleWindowLoad);
-
-    return () => {
-      window.removeEventListener("load", handleWindowLoad);
-    };
-  }, []);
-
   return (
     <React.Fragment>
-      {/* Start the loading animation */}
-      {isLoading && <Loader />}
-
-      {/* Main Website */}
       <NavigationBar />
     </React.Fragment>
   );
