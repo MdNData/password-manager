cc8d5dc9b9f615d9a844dd7ce4ce294ca390fefa
Dra <nd.mirica@gmail.com>
2023-07-12 20:49:43 +0300

    password list

--- src/components/PasswordList/PasswordList.js
+++ src/components/PasswordList/PasswordList.js
@@ -1,36 +1,89 @@
 //Individual password container
-const PasswordContainer = () => {
+const PasswordContainer = ({ data }) => {
+  const { siteSrc, siteName, username, pass } = data;
   return (
     <article>
       <div className="identifier">
-        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
+        <img src={siteSrc} alt="" />
         <p>
           Usage :
-          <input type="text" value="testontestbytestintest.com" />
+          <input type="text" defaultValue={siteName} />
         </p>
       </div>
       <div className="data-container">
         <p>
           User :
-          <input type="text" value="testontestbytestintest@gmail.com" />
+          <input type="text" defaultValue={username} />
         </p>
         <p>
           Password :
-          <input type="text" value="TestingonTest01.Tests" />
+          <input type="text" defaultValue={pass} />
         </p>
       </div>
     </article>
   );
 };
 
+//passwords row data container
+const listData = [
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+  {
+    siteSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+    siteName: "testontestbytestintest.com",
+    username: "testontestbytestintest@gmail.com",
+    pass: "TestingonTest01.Tests",
+  },
+];
+
 //Complete list of passwords to be displayed
 export const PasswordList = () => {
   return (
     <section className="password-container">
-      <PasswordContainer />
+      {listData.map((item, key) => {
+        return <PasswordContainer data={item} key={key} />;
+      })}
     </section>
+    
   );
 };
-
-//passwords row data container
-const passwordList = [{}];
