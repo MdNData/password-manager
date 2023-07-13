10d2913efe4c12f0ec9999fc813d6b50f47221bc
Dra <nd.mirica@gmail.com>
2023-07-13 17:48:20 +0300

    add realtime image icon update

--- src/components/PasswordList/NewPassword.js
+++ src/components/PasswordList/NewPassword.js
@@ -1,8 +1,30 @@
-export const NewPassword = ({className}) => {
+import { useState } from "react";
+
+export const NewPassword = ({ className }) => {
+  const [srcImg, setSrcImg] = useState(
+    "https://mdndata.github.io/password-manager/static/media/logo.b8ce593d759382a06a27.png"
+  );
+
+  const update = (e) => {
+    setSrcImg(e.target.value);
+  }
+  
   return (
     <article className={className}>
       <div className="identifier">
-        <img src="new entry" alt="" />
+        <img
+          src={srcImg}
+          alt=""
+        />
+        <p style={{ marginBottom: "20px" }}>
+          Image Link :
+          <input
+            type="text"
+            defaultValue="https://mdndata.github.io/password-manager/static/media/logo.b8ce593d759382a06a27.png"
+            onChange={update}
+          />
+        </p>
+        <br />
         <p>
           Usage :
           <input type="text" defaultValue="new entry" />
